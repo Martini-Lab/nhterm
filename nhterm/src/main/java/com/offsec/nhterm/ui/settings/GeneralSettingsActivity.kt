19d02b409f603beac1828f63a0592dd602bdb8b3
Martin Valba <kailing2@live.com>
2023-01-08 19:14:34 +0200

    Changes for settings
    * Remove un-related options from seetings that we wont use ( we use chroot system's su to open shell with proper env exports )
    * Some misc change for shortcuts option

--- nhterm/src/main/java/com/offsec/nhterm/ui/settings/GeneralSettingsActivity.kt
+++ nhterm/src/main/java/com/offsec/nhterm/ui/settings/GeneralSettingsActivity.kt
@@ -2,10 +2,7 @@ package com.offsec.nhterm.ui.settings
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import com.offsec.nhterm.R
-import com.offsec.nhterm.component.config.NeoPreference
-import com.offsec.nhterm.utils.runApt
 
 /**
  * @author kiva
@@ -17,34 +14,6 @@ class GeneralSettingsActivity : BasePreferenceActivity() {
     supportActionBar?.title = getString(R.string.general_settings)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
     addPreferencesFromResource(R.xml.setting_general)
-
-    val currentShell = NeoPreference.getLoginShellName()
-    findPreference(getString(R.string.key_general_shell)).setOnPreferenceChangeListener { _, value ->
-      val shellName = value.toString()
-      val newShell = NeoPreference.findLoginProgram(shellName)
-      if (newShell == null) {
-        requestInstallShell(shellName, currentShell)
-      } else {
-        postChangeShell(shellName)
-      }
-      return@setOnPreferenceChangeListener true
-    }
-  }
-
-  private fun postChangeShell(shellName: String) = NeoPreference.setLoginShellName(shellName)
-
-  private fun requestInstallShell(shellName: String, currentShell: String) {
-    AlertDialog.Builder(this)
-      .setTitle(getString(R.string.shell_not_found, shellName))
-      .setMessage(R.string.shell_not_found_message)
-      .setPositiveButton(R.string.install) { _, _ ->
-        runApt("install", "-y", shellName) {
-          it.onSuccess { postChangeShell(shellName) }
-        }
-      }
-      .setNegativeButton(android.R.string.no, null)
-      .setOnDismissListener { postChangeShell(currentShell) }
-      .show()
   }
 
   override fun onBuildHeaders(target: MutableList<Header>?) {
